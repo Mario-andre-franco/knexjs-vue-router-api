65ebd048c4a29355806e0fce2bbcd66943d765c2
Mario Franco <mario.franco@wdev.int>
2023-03-17 11:55:28 -0300

    config routes

---
+++ userApi/controllers/UserController.js
@@ -0,0 +1,10 @@
+class UserController {
+    async index(req,res) {}
+
+    async create(req,res) {
+        console.log(req.body)
+        res.send("pegando corpo")
+    }
+}
+
+module.exports = new UserController();
--- userApi/database/connection.js
+++ userApi/database/connection.js
@@ -1,11 +1,17 @@
-var knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'usertest'
-    }
-  });
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : process.env.PG_HOST,
+    port : 5432,
+    user : 'postgres',
+    password : process.env.PG_PASS,
+    database : 'user'
+  }
+});
 
 module.exports = knex
