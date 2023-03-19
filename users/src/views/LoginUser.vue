bae52f50898a703a6b9c4cdddd30b6de3a6ad080
Mario Franco <mario.andre.franco@gmail.com>
2023-03-19 10:22:39 -0300

    refactor login

--- users/src/views/LoginUser.vue
+++ users/src/views/LoginUser.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
     <h2>Registro de usuário!</h2>
-    <hr />
-
+    <p>E-mail</p>
+    <input
+      type="email"
+      placeholder="email@email.com"
+      class="input"
+      v-model="email"
+    />
+    <p>Senha</p>
+    <input
+      type="password"
+      placeholder="******"
+      class="input"
+      v-model="password"
+    />
     <div class="columns is-centered">
       <div class="column is-half">
         <div v-if="error != undefined">
           <div class="notification is-danger">
             {{ error }}
           </div>
+          <input
+            type="email"
+            placeholder="email@email.com"
+            class="input"
+            v-model="email"
+          />
+          <p>Senha</p>
+          <input
+            type="password"
+            placeholder="******"
+            class="input"
+            v-model="password"
+          />
         </div>
-
         <hr />
         <button class="button is-success" @click="login">Login</button>
       </div>
